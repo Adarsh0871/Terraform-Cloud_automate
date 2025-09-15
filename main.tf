a77bd23762123b7021f3d3479cf8348bd9501b36
Adarsh0871 <akmg0871@gmail.com>
2025-09-15 21:40:53 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,185 @@
+
+// Copyright (c) 2017, 2021, Oracle and/or its affiliates. All rights reserved.
+// Licensed under the Mozilla Public License v2.0
+
+variable "tenancy_ocid" {
+}
+
+variable "user_ocid" {
+}
+
+variable "fingerprint" {
+}
+
+variable "private_key" {
+}
+
+variable "ssh_public_key" {
+}
+
+variable "compartment_ocid" {
+}
+
+variable "region" {
+}
+
+variable "dbname" {
+  default     = "hbdemo01"
+}
+
+provider "oci" {
+  region           = var.region
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key      = var.private_key
+}
+
+variable "ad_region_mapping" {
+  type = map(string)
+
+  default = {
+    us-london-1 = 1
+  }
+}
+
+variable "images" {
+  type = map(string)
+
+  default = {
+    # See https://docs.us-phoenix-1.oraclecloud.com/images/
+    # Oracle-provided image "Oracle-Linux-7.5-2018.10.16-0"
+    uk-london-1    = "ocid1.image.oc1.uk-london-1.aaaaaaaaskspfz56rlcmtfbr2milotcxqcpitly63zipmn4joygm44qs7hua"
+  }
+}
+
+data "oci_identity_availability_domain" "ad" {
+  compartment_id = var.tenancy_ocid
+  ad_number      = var.ad_region_mapping[var.region]
+}
+
+resource "oci_core_virtual_network" "test_vcn" {
+  cidr_block     = "10.1.0.0/16"
+  compartment_id = var.compartment_ocid
+  display_name   = "testVCN"
+  dns_label      = "testvcn"
+}
+
+resource "oci_core_subnet" "test_subnet" {
+  cidr_block        = "10.1.20.0/24"
+  display_name      = "testSubnet"
+  dns_label         = "testsubnet"
+  security_list_ids = [oci_core_security_list.test_security_list.id]
+  compartment_id    = var.compartment_ocid
+  vcn_id            = oci_core_virtual_network.test_vcn.id
+  route_table_id    = oci_core_route_table.test_route_table.id
+  dhcp_options_id   = oci_core_virtual_network.test_vcn.default_dhcp_options_id
+}
+
+resource "oci_core_internet_gateway" "test_internet_gateway" {
+  compartment_id = var.compartment_ocid
+  display_name   = "testIG"
+  vcn_id         = oci_core_virtual_network.test_vcn.id
+}
+
+resource "oci_core_route_table" "test_route_table" {
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_virtual_network.test_vcn.id
+  display_name   = "testRouteTable"
+
+  route_rules {
+    destination       = "0.0.0.0/0"
+    destination_type  = "CIDR_BLOCK"
+    network_entity_id = oci_core_internet_gateway.test_internet_gateway.id
+  }
+}
+
+resource "oci_core_security_list" "test_security_list" {
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_virtual_network.test_vcn.id
+  display_name   = "testSecurityList"
+
+  egress_security_rules {
+    protocol    = "6"
+    destination = "0.0.0.0/0"
+  }
+
+  ingress_security_rules {
+    protocol = "6"
+    source   = "0.0.0.0/0"
+
+    tcp_options {
+      max = "22"
+      min = "22"
+    }
+  }
+
+  ingress_security_rules {
+    protocol = "6"
+    source   = "0.0.0.0/0"
+
+    tcp_options {
+      max = "3000"
+      min = "3000"
+    }
+  }
+
+  ingress_security_rules {
+    protocol = "6"
+    source   = "0.0.0.0/0"
+
+    tcp_options {
+      max = "3005"
+      min = "3005"
+    }
+  }
+
+  ingress_security_rules {
+    protocol = "6"
+    source   = "0.0.0.0/0"
+
+    tcp_options {
+      max = "80"
+      min = "80"
+    }
+  }
+}
+
+
+data "oci_core_images" "test_images" {
+  #Required
+  compartment_id = var.compartment_ocid
+
+  #Optional
+  shape = "VM.Standard.A1.Flex"
+}
+
+data "oci_database_autonomous_databases" "test_autonomous_databases" {
+  #Required
+  compartment_id = var.compartment_ocid
+
+  #Optional
+  db_workload  = "OLTP"
+  is_free_tier = "true"
+}
+
+resource "oci_database_autonomous_database" "test_autonomous_database" {
+  #Required
+  admin_password           = "Testalwaysfree1"
+  compartment_id           = var.compartment_ocid
+  cpu_core_count           = "1"
+  data_storage_size_in_tbs = "1"
+  db_name                  = var.dbname
+
+  #Optional
+  db_workload  = "OLTP"
+  display_name = var.dbname
+
+  freeform_tags = {
+    "Department" = "Finance"
+  }
+
+  is_auto_scaling_enabled = "false"
+  license_model           = "LICENSE_INCLUDED"
+  is_free_tier            = "true"
+}
